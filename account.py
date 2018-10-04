f3d3cdd06dc16c2080d415d93c9502d546252305
alexmlopez <alxmlpz@gmail.com>
2018-10-04 04:37:31 +0000

    new code

---
+++ account.py
@@ -0,0 +1,9 @@
+        if len(password) > 8:
+            return True
+        else:
+            return False
+
+if __name__ == '__main__':
+    accVerify = account()
+    print('The password length is ' + str(accVerify.check_password_length('offtoschool')))
+
